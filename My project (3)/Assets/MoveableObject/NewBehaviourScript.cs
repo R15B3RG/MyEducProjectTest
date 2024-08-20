e4f8471a7b5abb4fe972b3ea6d635e4acd3b2d12
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-08-20 15:08:09 +0200

    Step 2

--- My project (3)/Assets/MoveableObject/NewBehaviourScript.cs
+++ My project (3)/Assets/MoveableObject/NewBehaviourScript.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Vector3 = UnityEngine.Vector3;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    [SerializeField] private GameObject _gameObject;
+    
+
+    Vector3 vector = new Vector3();
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("It's working!");
+
+        Transform myTransform = transform;
+
+        Vector3 myPosition = transform.position;
+
+        transform.position += new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+
+
     }
 }
