d2aeef61d07632297a3426760daefad43afeede5
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-08-21 16:21:18 +0200

    latest

--- My project (3)/Assets/Obstacle/Obstacle.cs
+++ My project (3)/Assets/Obstacle/Obstacle.cs
@@ -1,25 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Threading;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
 
-public class ExplodeOnImpact : MonoBehaviour
+
+public class Obstacle : MonoBehaviour
 {
     [SerializeField] private GameObject _obstacle;
     // Start is called before the first frame update
+
+    public float Speed = 5.0f;
+
+    public float RandPosX;
+
+    public float RandPosZ;
+
+    private Vector3 _newPosition;
+
+    private Vector3 _initPosition;
+
     void Start()
     {
+        _initPosition = transform.position;
 
-        Transform myTransform = transform;
+        RandPosX = Random.Range(-1.5f, 1.5f);
 
-        Vector3 myPosition = transform.position;
+        RandPosZ = Random.Range(-1.5f, 1.5f);
 
-        transform.position += new Vector3(0, 0, 0);
+        _newPosition = new Vector3(RandPosX, 0, RandPosZ);
     }
+
 
     void Update()
     {
+
+        transform.position += new Vector3(_newPosition.x, 0, _newPosition.z) * Time.deltaTime * Speed;
+
+        if (Vector3.Distance(transform.position, _newPosition) <= 1)
+        {
+            
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _initPosition, Speed * Time.deltaTime);
 
     }
 }
