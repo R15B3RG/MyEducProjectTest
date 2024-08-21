1c2b168b6717616b71ab4df9f97361a2293778e0
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-08-21 10:09:06 +0200

    1a - 1d done

--- My project (3)/Assets/MoveableObject/ExplodeOnImpact.cs
+++ My project (3)/Assets/MoveableObject/ExplodeOnImpact.cs
@@ -27,9 +27,54 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 0, 3) * Time.deltaTime;
+        //transform.position += new Vector3(0, 0, 3) * Time.deltaTime;
 
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 3) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -3) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(3, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-3, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += new Vector3(0, 7, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 7) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -7) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(7, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-7, 0, 0) * Time.deltaTime;
+        }
+
+
     }
 
     void OnCollisionEnter(Collision coll)
