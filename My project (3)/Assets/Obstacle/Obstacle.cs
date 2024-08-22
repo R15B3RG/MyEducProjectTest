0727250d07672624e5a7ac9101cc88aa76a2c902
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-08-22 12:52:27 +0200

    2b

--- My project (3)/Assets/Obstacle/Obstacle.cs
+++ My project (3)/Assets/Obstacle/Obstacle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
@@ -12,14 +13,14 @@ public class Obstacle : MonoBehaviour
 {
     [SerializeField] private GameObject _obstacle;
 
-    
+
     // Start is called before the first frame update
 
     public float Speed = 3f;
 
-    public float RandPosX;
+    public float MinVal = -10f;
 
-    public float RandPosZ;
+    public float MaxVal = 10f;
 
     private Vector3 _newPosition;
 
@@ -31,36 +32,44 @@ public class Obstacle : MonoBehaviour
     {
         _initPosition = transform.position;
 
-        RandPosX = Random.Range(-1.5f, 1.5f);
+        SetNewRandomPosition();
 
-        RandPosZ = Random.Range(-1.5f, 1.5f);
+    }
 
-        _newPosition = new Vector3(RandPosX, 0, RandPosZ);
+    void SetNewRandomPosition()
+    {
+
+        _newPosition = _initPosition + new Vector3(Random.Range(MinVal, MaxVal), 0, Random.Range(MinVal, MaxVal));
+
+        Debug.Log(_newPosition);
     }
 
 
     void Update()
     {
 
-        while (true)
+        if (_isMoving)
         {
-            if (_isMoving)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, _newPosition, Speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, _newPosition, Speed * Time.deltaTime);
 
-                if (Vector3.Distance(transform.position, _newPosition) <= 2)
-                {
-                    _isMoving = false;
-                }
-            }
-            else
+            if (Vector3.Distance(transform.position, _newPosition) <= 2)
             {
-                transform.position = Vector3.MoveTowards(transform.position, _initPosition, Speed * Time.deltaTime);
+                _isMoving = false;
             }
         }
-        //Vector3.Distance(transform.position, _newPosition) <= 2
-        //transform.position += new Vector3(_newPosition.x, 0, _newPosition.z) * Speed * Time.deltaTime;
-        //transform.position = Vector3.MoveTowards(transform.position, _initPosition, Speed * Time.deltaTime);
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _initPosition, Speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, _initPosition) <= 1)
+            {
+                SetNewRandomPosition();
+
+                _isMoving = true;
+
+               
+            }
+        }
 
     }
 }
