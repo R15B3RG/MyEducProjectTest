75b7c8c01965efc2cd49b040bac7097f615ec7a7
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-08-29 14:56:23 +0200

    update

---
+++ My project (3)/Assets/EndPointLevel/EndPoint.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Time = UnityEngine.Time;
+
+public class EndPoint : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _objectsToEnable;
+
+    [SerializeField] private GameObject _nextLevelUi;
+
+    [SerializeField] private GameObject _endPoint;
+
+    [SerializeField] private GameObject _textScript;
+
+    [SerializeField] private Button _nextLevel;
+
+    private const string _nextLevelName = "Level2";
+
+    private bool _enableObjects = false;
+
+    private void Awake()
+    {
+        _nextLevel.onClick.AddListener(OnNextLevel);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnNextLevel()
+    {
+        SceneManager.LoadScene(_nextLevelName);
+
+        Time.timeScale = 1;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            _enableObjects = !_enableObjects;
+            _objectsToEnable.ForEach(go => go.SetActive(_enableObjects));
+            Time.timeScale = 0;
+        }
+    }
+
+}
